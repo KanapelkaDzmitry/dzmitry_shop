8dc2b4846bfbaa654e977f6fc8435ab2a458c6d5
mkanapelka <dkonopelko748@gmail.com>
2022-01-22 15:06:17 +0300

    init commit

---
+++ src/main/java/com/example/dzmitry_shop/DzmitryShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dzmitry_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzmitryShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzmitryShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dzmitry_shop/DzmitryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dzmitry_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzmitryShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

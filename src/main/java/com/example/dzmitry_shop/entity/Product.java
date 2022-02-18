9cebbcbb7e7f59706bcdfda8032961a67386a5fc
dkanapelka <dkonopelko748@gmail.com>
2022-02-19 02:29:49 +0300

    Added 4 classes but can't figure out my mistakes

--- src/main/java/com/example/dzmitry_shop/entity/Product.java
+++ src/main/java/com/example/dzmitry_shop/entity/Product.java
@@ -1,4 +1,32 @@
 package com.example.dzmitry_shop.entity;
 
-public class Product {
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Product extends BaseEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private int sellerCode;
+
+    private int cost;
+    private int quantity;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "subCategory_id")
+    private SubCategory subCategory;
 }
---
+++ src/main/java/com/example/dzmitry_shop/entity/SubCategory.java
@@ -0,0 +1,24 @@
+package com.example.dzmitry_shop.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class SubCategory extends BaseEntity{
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+    private boolean isActive;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Category_id")
+    private Category category;
+}
